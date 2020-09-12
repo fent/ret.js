8854cf863bbb57ea85107574a8a80870bb00db00
Jesse Wright <jesse@jesse-hp-laptop.local>
2020-09-12 21:25:31 +1000

    removing dist folder

--- dist/positions.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var types = require('./types');
-exports.wordBoundary = function () { return ({ type: types.POSITION, value: 'b' }); };
-exports.nonWordBoundary = function () { return ({ type: types.POSITION, value: 'B' }); };
-exports.begin = function () { return ({ type: types.POSITION, value: '^' }); };
-exports.end = function () { return ({ type: types.POSITION, value: '$' }); };

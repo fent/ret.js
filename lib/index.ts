324fe8600c120d9a5f260fa89f162d4c51bb0bca
Jesse Wright <jesse.wright@anu.edu.au>
2020-11-30 14:10:41 +1100

    Adding `module.exports.reconstruct`
    Needed to make importing using the syntax shown in the README valid

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@ import { types } from './types';
 export * from './tokenizer';
 export * from './reconstruct';
 import { tokenizer } from './tokenizer';
+import { reconstruct } from './reconstruct';
 export * from './types';
 
 export default tokenizer;
@@ -9,3 +10,4 @@ export { types };
 
 module.exports = tokenizer;
 module.exports.types = types;
+module.exports.reconstruct = reconstruct;

0a9252af61e9ae23c6cf076f1d5d99860b01c2bc
jeswr <jesse.wright@anu.edu.au>
2021-01-06 11:58:25 +1100

    Rename boolean variables

--- lib/write-set-tokens.ts
+++ lib/write-set-tokens.ts
@@ -4,16 +4,16 @@ import * as sets from './sets-lookup';
 /**
  * Takes character code and returns character to be displayed in a set
  * @param {number} charCode Character code of set element
- * @param {boolean} firstChar True if character is the first character of the set
- * @param {boolean} lastChar True if character is the last character of the set
+ * @param {boolean} isFirstChar True if character is the first character of the set
+ * @param {boolean} isLastChar True if character is the last character of the set
  * @returns {string} The string for the sets character
  */
-export function setChar(charCode: number, firstChar: boolean, lastChar = false): string {
+export function setChar(charCode: number, isFirstChar: boolean, isLastChar = false): string {
   // We only need to negate first internal '^' if token.not is false
-  return firstChar && charCode === 94 ? '\\^' :
+  return isFirstChar && charCode === 94 ? '\\^' :
     charCode === 92 ? '\\\\' :
       charCode === 93 ? '\\]' :
-        charCode === 45 && !lastChar ? '\\-' :
+        charCode === 45 && !isLastChar ? '\\-' :
           String.fromCharCode(charCode);
 }
 
@@ -23,7 +23,7 @@ export function setChar(charCode: number, firstChar: boolean, lastChar = false):
  * @param {Record<string | number, boolean>} map The predefined 'set-lookup'
  * @returns {boolean} True if the character set corresponds to the 'set-lookup'
  */
-function sameSet(set: SetTokens, map: Record<string | number, boolean>): boolean {
+function isSameSet(set: SetTokens, map: Record<string | number, boolean>): boolean {
   for (const elem of set) {
     if (elem.type === types.SET) {
       return false;
@@ -41,11 +41,11 @@ function sameSet(set: SetTokens, map: Record<string | number, boolean>): boolean
 /**
  * Writes the tokens for a set
  * @param {Set} set The set to display
- * @param {boolean} firstChar Whether subset contains the first character of the set
- * @param {boolean} nested Whether the token is nested inside another set token
+ * @param {boolean} isFirstChar Whether subset contains the first character of the set
+ * @param {boolean} isNested Whether the token is nested inside another set token
  * @returns {string} The tokens for the set
  */
-export function writeSetTokens(set: Set, firstChar: boolean, nested = false): string {
+export function writeSetTokens(set: Set, isFirstChar: boolean, isNested = false): string {
   const len = set.set.length;
   if (len === 1) {
     // Ints case
@@ -54,45 +54,45 @@ export function writeSetTokens(set: Set, firstChar: boolean, nested = false): st
       return set.not ? '\\D' : '\\d';
     }
   } else if (len === 4) {
-    if (sameSet(set.set, sets.WORDS())) {
+    if (isSameSet(set.set, sets.WORDS())) {
       return set.not ? '\\W' : '\\w';
     }
     // Notanychar is only relevant when not nested inside another set token
-    if (!nested && set.not && sameSet(set.set, sets.NOTANYCHAR())) {
+    if (!isNested && set.not && isSameSet(set.set, sets.NOTANYCHAR())) {
       return '.';
     }
   } else if (len === 15 && sets.WHITESPACE()) {
     return set.not ? '\\S' : '\\s';
   }
-  let firstCharTemp = firstChar;
+  let isFirstCharTemp = isFirstChar;
   let tokenString = '';
   for (let i = 0; i < len; i++) {
     const subset = set.set[i];
-    tokenString += writeSetToken(subset, firstCharTemp, i === len - 1);
+    tokenString += writeSetToken(subset, isFirstCharTemp, i === len - 1);
     // Only cancel out firstChar condition when we reach first non empty
     // set in the sequence
-    firstCharTemp = firstCharTemp && subset.type === types.SET && subset.set.length === 0;
+    isFirstCharTemp = isFirstCharTemp && subset.type === types.SET && subset.set.length === 0;
   }
   const contents = `${set.not ? '^' : ''}${tokenString}`;
-  return nested ? contents : `[${contents}]`;
+  return isNested ? contents : `[${contents}]`;
 }
 
 /**
  * Writes a token within a set
  * @param {Range | Char | Set} set The set token to display
- * @param {boolean} firstChar Whether subset contains the first character of the set
- * @param {boolean} lastChar True if character is the last character of the set
+ * @param {boolean} isFirstChar Whether subset contains the first character of the set
+ * @param {boolean} isLastChar True if character is the last character of the set
  * @returns {string} The token as a string
  */
-function writeSetToken(set: Range | Char | Set, firstChar: boolean, lastChar: boolean): string {
+function writeSetToken(set: Range | Char | Set, isFirstChar: boolean, isLastChar: boolean): string {
   if (set.type === types.CHAR) {
-    return setChar(set.value, firstChar, lastChar);
+    return setChar(set.value, isFirstChar, isLastChar);
   } else if (set.type === types.RANGE) {
     if (set.from === 48 && set.to === 57) {
       return '\\d';
     } else {
-      return `${setChar(set.from, firstChar)}-${setChar(set.to, false, true)}`;
+      return `${setChar(set.from, isFirstChar)}-${setChar(set.to, false, true)}`;
     }
   }
-  return writeSetTokens(set, firstChar, true);
+  return writeSetTokens(set, isFirstChar, true);
 }
